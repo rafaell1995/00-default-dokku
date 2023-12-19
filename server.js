69161ea4c90cde0ae69bfadee35214a096e5ea14
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-12-19 00:19:31 -0300

    Fix port to 5000

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000; // O Dokku define a variável PORT
+const port = process.env.PORT || 5000; // O Dokku define a variável PORT
 
 // Servir arquivos estáticos da pasta 'public'
 app.use(express.static('public'));
